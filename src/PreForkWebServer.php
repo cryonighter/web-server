b02a0d9aad3a846af9e991c3b7e0eb7be7776d96
cryonighter <cryonighter@yandex.ru>
2026-01-28 01:06:18 +0300

    Переделал работу с воркерами на асинхронную модель

--- src/PreForkWebServer.php
+++ src/PreForkWebServer.php
@@ -39,6 +39,7 @@ class PreForkWebServer extends WebServer
         pcntl_signal(SIGTERM, fn() => $this->handleShutdown());
         pcntl_signal(SIGINT, fn() => $this->handleShutdown());
         pcntl_signal(SIGQUIT, fn() => $this->handleShutdown());
+        pcntl_signal(SIGCHLD, fn() => $this->handleWorkerExit($sockets, $hostConfigs));
 
         try {
             for ($i = 1; $i <= self::WORKER_COUNT; $i++) {
@@ -47,50 +48,20 @@ class PreForkWebServer extends WebServer
 
             $this->logger->info('All workers spawned');
 
-            $workerInfoIteration = 0;
-
             while (!$this->shutdown) {
-                // Проверка работоспособности воркеров
+                // Проверка сообщений от воркеров (каждые 5 секунд)
                 foreach ($this->workers as $id => $workerInfo) {
-                    $status = 0;
+                    $ipcInfo = $workerInfo->ips->read();
 
-                    if (pcntl_waitpid($workerInfo->pid, $status, WNOHANG) > 0) {
-                        $signalCode = pcntl_wifsignaled($status) ? pcntl_wtermsig($status) : null;
-                        $exitCode = pcntl_wexitstatus($status);
-
-                        if ($signalCode) {
-                            $this->logger->warning("Worker #$id (PID: $workerInfo->pid) killed by signal: $signalCode");
-                        } else {
-                            $this->logger->info("Worker #$id (PID: $workerInfo->pid) exited with code: $exitCode");
-                        }
-
-                        $workerInfo->ips->close();
-
-                        unset($this->workers[$id]);
-
-                        if (!$this->shutdown) {
-                            $this->logger->info("Respawning worker #$id");
-                            $this->fork($sockets, $hostConfigs, $id);
-                        }
+                    if ($ipcInfo) {
+                        $this->logger->debug("Worker #$id processed $ipcInfo->requests requests");
+                        $this->logger->debug("Worker #$id used $ipcInfo->memory bytes memory");
+                    } else {
+                        $this->logger->warning("Worker #$id didn't send any data");
                     }
                 }
 
-                // Проверка сообщений от воркеров (пока просто чтобы было, позже придумаю назначение)
-                if (++$workerInfoIteration >= 50) { // Каждые 5 секунд
-                    foreach ($this->workers as $id => $workerInfo) {
-                        $ipcInfo = $workerInfo->ips->read();
-
-                        if ($ipcInfo) {
-                            $this->logger->debug("Worker #$id processed $ipcInfo->requests requests");
-                            $this->logger->debug("Worker #$id used $ipcInfo->memory bytes memory");
-                        } else {
-                            $this->logger->warning("Worker #$id didn't send any data");
-                        }
-                    }
-                    $workerInfoIteration = 0;
-                }
-
-                usleep(100000); // 0.1s
+                usleep(5000000); // 5s
             }
         } catch (Throwable $exception) {
             $this->logger->error($exception);
@@ -135,7 +106,7 @@ class PreForkWebServer extends WebServer
                     if ($this->shutdown) {
                         $this->logger->info('Stopped worker by signal from master');
                     } else {
-                        $this->logger->info('Stopped worker by s reached max requests limit (' . self::MAX_REQUESTS_PER_WORKER . ')');
+                        $this->logger->info('Stopped worker by reached max requests limit (' . self::MAX_REQUESTS_PER_WORKER . ')');
                     }
                     $ips->close();
                 },
@@ -158,37 +129,66 @@ class PreForkWebServer extends WebServer
         $this->logger->info('Stopping all workers...');
 
         // Даем воркерам 10 секунд на graceful shutdown
-        $timeout = time() + 10;
-        while (!empty($this->workers) && time() < $timeout) {
-            foreach ($this->workers as $id => $workerInfo) {
-                if (pcntl_waitpid($workerInfo->pid, $status, WNOHANG) > 0) {
-                    $this->logger->info("Worker #$id stopped");
-
-                    $workerInfo->ips->close();
-
-                    unset($this->workers[$id]);
-                }
-            }
-            usleep(100000);
-        }
+        $this->waitStopingWorkers(10, 100000);
 
         // Принудительно убиваем оставшихся воркеров
         foreach ($this->workers as $id => $workerInfo) {
             $this->logger->warning("Force killing worker #$id (PID: $workerInfo->pid)");
 
             posix_kill($workerInfo->pid, SIGKILL);
-            pcntl_waitpid($workerInfo->pid, $status);
-
-            $this->logger->info("Worker #$id killed");
-
-            $workerInfo->ips->close();
-
-            unset($this->workers[$id]);
         }
+
+        // Ждем принудительной остановки еще 2 секунды
+        $this->waitStopingWorkers(2, 50000);
 
         // Закрываем сокеты
         foreach ($sockets as $socket) {
             fclose($socket);
+        }
+
+        if (!empty($this->workers)) {
+            throw new RuntimeException('Failed killing workers: ' . implode(', ', array_keys($this->workers)));
+        }
+    }
+
+    private function waitStopingWorkers(int $timeout, int $interval): void
+    {
+        $timeout = time() + $timeout;
+        while (!empty($this->workers) && time() < $timeout) {
+            usleep($interval);
+        }
+    }
+
+    private function handleWorkerExit(array $sockets, array $hostConfigs): void
+    {
+        // Обрабатываем все завершившиеся процессы
+        while (($pid = pcntl_waitpid(-1, $status, WNOHANG)) > 0) {
+            $workerId = array_find_key($this->workers, fn(WorkerInfo$workerInfo): bool => $workerInfo->pid === $pid);
+
+            if ($workerId === null) {
+                $this->logger->warning("Unknown process with PID $pid exited");
+                continue;
+            }
+
+            $workerInfo = $this->workers[$workerId];
+
+            $signalCode = pcntl_wifsignaled($status) ? pcntl_wtermsig($status) : null;
+            $exitCode = pcntl_wexitstatus($status);
+
+            if ($signalCode) {
+                $this->logger->warning("Worker #$workerId (PID: $pid) killed by signal: $signalCode");
+            } else {
+                $this->logger->info("Worker #$workerId (PID: $pid) exited with code: $exitCode");
+            }
+
+            $workerInfo->ips->close();
+
+            unset($this->workers[$workerId]);
+
+            if (!$this->shutdown) {
+                $this->logger->info("Respawning worker #$workerId");
+                $this->fork($sockets, $hostConfigs, $workerId);
+            }
         }
     }
 }
